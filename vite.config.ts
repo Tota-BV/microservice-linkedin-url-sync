48dade112e4112521ee7c2649674b98897f15e8a
Julian Klumpers <julianklumpers@Mac.orbit>
2025-07-28 21:48:37 +0200

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import path from "path";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "src"),
+		},
+	},
 	plugins: [
 		viteTsConfigPaths({
 			projects: ["./tsconfig.json"],
